9fe8c6c3755fa58741a56b81f11d8f0ce00ba28e
robo-taisho <gengaozo.god@gmail.com>
2014-12-23 17:15:34 +0900

    二つのソートの練習

---
+++ qsort1223.py
@@ -0,0 +1,33 @@
+#coding : UTF-8
+
+import random
+
+def generate(n):
+  return [random.randint(0,100) for _ in xrange(n)]
+
+def qsort(alist):
+  if len(alist) < 2:
+    return alist
+  else:
+    left = []
+    right = []
+    for i in range(1, len(alist)):
+      if alist[i] > alist[0]:
+        right.append(alist[i])
+      else:
+        left.append(alist[i])
+
+  left = qsort(left)
+  right= qsort(right)
+
+  ans = []
+  ans += left
+  ans.append(alist[0])
+  ans += right
+
+  return ans
+
+
+alist = generate(20)
+print alist
+print qsort(alist)
